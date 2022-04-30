693ec07bc538cc4ca66abd17e8650a20980972fb
ytgw <tagawa0920@gmail.com>
2022-04-30 20:53:47 +0900

    もっとビューを書いてみる。

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You'are at the polls index.")
+
+def detail(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request: HttpRequest, question_id: int) -> HttpResponse:
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request: HttpRequest, question_id: int) -> HttpResponse:
+    return HttpResponse("You're voting on question %s." % question_id)
