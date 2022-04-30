9430ae9c48274603669184697a465cc3340b599e
ytgw <tagawa0920@gmail.com>
2022-04-30 17:40:37 +0900

    関数indexに型を追加。

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello, world. You'are at the polls index.")
